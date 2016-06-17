ea9aac218e1954d5572ba0e6449bde2a7fade1b1
aureliome <aurelio.me@gmail.com>
2016-06-17 17:35:14 +0200

    Version 1.0.2

---
+++ docs/1.0.2/js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "version": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "version",
+      "id": "Callbacky",
+      "shortName": "Callbacky",
+      "type": "object",
+      "moduleName": "Callbacky",
+      "shortDescription": "Callbacky is a Javascript event handler that allows multiple parts of an application communicate with each other",
+      "keywords": "allows application arguments associate associated bind binded call callback callbacks callbacky called clean cleanall communicate configuration console debug error event eventname events executes function handler identify info init initialize javascript key log logger method methods methods_bind methods_init methods_trigger module multiple object options parts pass string trigger version warn world"
+    }
+  ],
+  "apis": {
+    "version": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/version/Callbacky",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
